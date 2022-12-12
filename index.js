13949b571350764924e9de82a5cf8fdfe008e0b6
aaveggupta <aaveggupta.contact@gmail.com>
2022-12-12 05:39:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const format = require("date-format");
+const app = express();
+const PORT = 4000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+app.get("/api/v1/linkedin", (req, res) => {
+  const linkedinStats = {
+    username: "aaveggupta",
+    followers: 785,
+    following: 643,
+    date: format.asString("dd/MM/yyyy - hh:mm:ss", new Date()),
+  };
+
+  res.status(200).json({ linkedinStats });
+});
+
+app.get("/api/v1/:token", (req, res) => {
+  res.status(200).json({ param: req.params.token });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running as ${PORT}`);
+});
